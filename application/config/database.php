6d76ef48a99485ae97162c4912b5635db4cdba7f
Alexandra <alexandra.e@hotmail.co.uk>
2020-01-29 15:11:05 +0000

    added database settings

--- application/config/database.php
+++ application/config/database.php
@@ -75,11 +75,11 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '',
-	'username' => '',
+	'hostname' => 'localhost',
+	'username' => 'root',
 	'password' => '',
-	'database' => '',
-	'dbdriver' => '',
+	'database' => 'gamereview',
+	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
