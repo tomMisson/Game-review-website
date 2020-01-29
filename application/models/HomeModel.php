bcde2b39ecf558d1b637fd3ab298aa82de2fad41
Alexandra <alexandra.e@hotmail.co.uk>
2020-01-29 15:01:48 +0000

    added navbar and started to implement database

--- application/models/HomeModel.php
+++ application/models/HomeModel.php
@@ -3,15 +3,15 @@ class HomeModel extends CI_Model{
 
     public function __construct()
     {
-        // $this->load->database();
+        $this->load->database();
     }
 
     //Get for all games
     public function getGame()
     {
         // You can use the select, from, and where functions to simplify this as seen in Week 13.
-        // $query = $this->db->query("SELECT * FROM activereviews");
-        // return $query->result();
+        $query = $this->db->query("SELECT * FROM activereviews");
+        return $query->result();
     }
 
     //Get the details for a game once it has been clicked on.
--- application/views/home.php
+++ application/views/home.php
@@ -4,57 +4,72 @@
 <!-- Required meta tags -->
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-<!-- Link CSS -->
-
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 <title><?php echo $title?></title>
-    <!-- These classes onlywork if you attach Bootstrap. -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <!-- Add your code here to create a nav bar -->
-      <!-- You can use code from your previous assignment -->
-  </nav>
 </head>
+<body>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">Games Reviews</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-<?php
-// Manipulate the body CSS colour here.
-?>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+        </ul>
 
-<div class="container">
-    <div class="row">
-        <?php
-        foreach ($result as $row)
-        {
-            // These classes onlywork if you attach Bootstrap.
-            echo '<div class="card cardBodyWidth '.$cssBodyClass.'">';
-            // This is presuming you have a column in your database table called ReviewImage.
-            $thisImage = $row->ReviewImage;
-            // Look into the image properties library in CodeIgniter for more help on images: 
-            
-        }
-        ?>
+        <form class="form-inline my-2 my-lg-0" method="POST">
+            <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Login</button>
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+  </header>
+
+  <main>
+    <div class="container">
+        <div class="row">
+            <?php
+            foreach ($result as $row)
+            {
+                echo '<div class="card cardBodyWidth '.$cssBodyClass.'">';
+                // This is presuming you have a column in your database table called ReviewImage.
+                $thisImage = $row->ReviewImage;
+                // Look into the image properties library in CodeIgniter for more help on images: 
+                
+            }
+            ?>
+        </div>
     </div>
-</div>
+  </main>
+
+  <footer class="page-footer font-small grey">
+    <div class="footer-copyright text-center py-3">
+      <img src="https://upload.wikimedia.org/wikipedia/en/thumb/9/94/Manchester_Metropolitan_University_logo.svg/220px-Manchester_Metropolitan_University_logo.svg.png">
+    </div>
+    <button id="chatButton" class="open-button btn btn-success" onclick="openForm()">Chat</button>
+    <div class="chat-popup pull-right" id="myForm">
+      <form id="myform" class="form-container">
+      </form>
+    </div>
+  </footer>
+
 </body>
 
 <!-- This section needs editing to create the chat system using HTML -->
-<button id="chatButton" class="open-button btn btn-success" onclick="openForm()">Chat</button>
-<div class="chat-popup pull-right" id="myForm">
-<form id="myform" class="form-container">
-</form>
-</div>
 
-<!-- Load in the required scripts -->
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-<!-- Don't forget to load in Vue abd socket.io -->
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/1.4.5/socket.io.min.js"></script>
 
-
-
-
-
-<!-- Load in your custom scripts -->
 
 </html>
